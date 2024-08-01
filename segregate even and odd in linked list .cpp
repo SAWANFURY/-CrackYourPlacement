6bc80c870e18f804674ee30395560951a95a6331
SAWANFURY <sawan.kr4849@gmail.com>
2024-08-01 19:45:14 +0530

    updated

---
+++ segregate even and odd in linked list .cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+// User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* divide(Node* head) {
+        // code here
+        Node * evenhead = NULL; Node * oddhead = NULL;
+        Node * eventail = NULL; Node * oddtail = NULL;
+        Node * current = head;
+        while(current){
+            if(current -> data % 2 == 0){
+                if(!evenhead){
+                    evenhead = eventail = current;
+                }
+                else{
+                    eventail -> next = current;
+                    eventail = eventail -> next;
+                }
+            }
+            else{
+                if(!oddhead){
+                    oddhead = oddtail = current;
+                }
+                else{
+                    oddtail -> next = current;
+                    oddtail = oddtail->next;
+                }
+            }
+           current = current -> next; 
+        }
+        if(eventail) eventail->next = oddhead;
+        if(oddtail) oddtail -> next = NULL;
+        head = (evenhead) ? evenhead : oddhead;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore(); // To ignore the newline character after the integer input
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty())
+            continue;
+
+        int data = arr[0];
+        int N = arr.size();
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 1; i < N; ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        Node *ans = ob.divide(head);
+        printList(ans);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
